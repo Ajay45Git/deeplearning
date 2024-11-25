08f19be7eeaca7a04a5657ff2c085710b467feab
Ajay45Git <ajay45saroj@gmail.com>
2024-11-25 14:54:19 +0530

    Update main.py
    initial commit 2:55

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import streamlit as st
-st.write("hfbdf")
+from imagetotext import image_to_text_page
+from imageclassification import image_classification_page  # Assuming you have a module for image classification
+
+def main():
+    # Sidebar for navigation
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ["Image Classification", "Image to Text"])
+
+    # Load the appropriate page based on sidebar selection
+    if page == "Image Classification":
+        image_classification_page()
+    elif page == "Image to Text":
+        image_to_text_page()
+
+if __name__ == "__main__":
+    main()
